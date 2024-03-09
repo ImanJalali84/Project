a23fbd53132d8913ff4553af9d0271774312b9e1
ImanJalali84 <161568365+ImanJalali84@users.noreply.github.com>
2024-03-09 21:15:15 +0330

    Ninth Day

--- asli.js
+++
@@ -1,9 +0,0 @@
-// تابعی برای افزودن محصول به سبد خرید
-function addToCart(productName) {
-    // اینجا می‌توانید کد اضافه کردن محصول به سبد خرید را پیاده‌سازی کنید
-    // به عنوان مثال:
-    // alert('محصول ' + productName + ' به سبد خرید اضافه شد!');
-    
-    // مثال: انتقال به صفحه سبد خرید با استفاده از URL
-    window.location.href = "cart.html?asli=" + encodeURIComponent(productName);
-}
